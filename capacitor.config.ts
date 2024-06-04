d6410b1f9e6b919482db157edf688e174a1aed9e
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2024-06-04 19:09:29 -0400

    more updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Simple Task Management',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
